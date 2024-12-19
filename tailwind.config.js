9387f6091532f7b125797d5f7f79a047d70e3671
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2024-12-19 15:40:24 +0600

    deployment commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-  content: [
-    "./components/**/*.{html,js}",
-    "./pages/**/*.{html,js}",
-    "./index.html",
-    "./shopPage.html",
-  ],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+        roboto: ["Roboto", "sans-serif"],
       },
     },
-    plugins: [],
   },
+  plugins: [],
 };
